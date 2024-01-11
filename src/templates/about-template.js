e5eba9e4a910d014815328aef1bcd94aa23fa36b
Fanny <fannyhedmans@gmail.com>
2024-01-11 17:02:29 +0100

    Navbar

--- src/templates/about-template.js
+++ src/templates/about-template.js
@@ -3,7 +3,9 @@ import * as React from 'react'
 const AboutTemplate = (contentfulPage) => {
   return (
     <main>
+      <div style={{marginTop: '100px'}}>
       <h2>{contentfulPage.title}</h2>
+      </div>
     </main>
   )
 }
